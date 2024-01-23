9f8ed42bc9350ffbab6751c798c94a16298a79c1
Arthur Rabelo <arthurrabelopro@gmail.com>
2024-01-23 14:04:09 -0300

    [upd:refactor] refactor app to remove navigation into a unique component

--- app.vue
+++ app.vue
@@ -1,13 +1,7 @@
 <!-- @format -->
 
 <template>
-  <nav>
-    <ul>
-      <li><NuxtLink to="/">Main</NuxtLink></li>
-      <li><NuxtLink to="/about">About</NuxtLink></li>
-      <li><a href="/about">About with a tag</a></li>
-    </ul>
-  </nav>
+  <Navbar />
   <div>
     <NuxtPage />
   </div>
---
+++ components/Navbar.vue
@@ -0,0 +1,18 @@
+<!-- @format -->
+
+<template>
+  <nav>
+    <ul>
+      <li><NuxtLink to="/">Main</NuxtLink></li>
+      <li><NuxtLink to="/about">About</NuxtLink></li>
+      <li><a href="/about">About with a tag</a></li>
+    </ul>
+  </nav>
+</template>
+
+<style>
+  ul {
+    display: flex;
+    gap: 20px;
+  }
+</style>
