b6bc5320347d21f6db9e2db20dc84b709d960335
Arthur Rabelo <arthurrabelopro@gmail.com>
2024-01-23 14:04:30 -0300

    [add:feat] display slug into the dynamic blog route

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -1,5 +1,12 @@
 <!-- @format -->
 
+<script setup lang="ts">
+  const route = useRoute();
+  console.log(route.params.slug);
+</script>
+
 <template>
-  <h1>We're inside of a blog post</h1>
+  <h1>
+    We're inside of blog post number <span>{{ route.params.slug }}</span>
+  </h1>
 </template>
